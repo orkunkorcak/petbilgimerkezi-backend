28dc23f95ea86a75420ddcb60210d3f68bb50739
your_name <orkunkorcak@gmail.com>
2025-09-25 23:56:46 +0300

    added cors and serverjs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import express from 'express';
+import { startServer } from './server.js';
 
-const app = express();
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+startServer();
