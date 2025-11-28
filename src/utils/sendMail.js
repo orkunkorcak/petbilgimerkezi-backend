0ff2a7180d048157bf21cfde3624f477c3b151a7
your_name <orkunkorcak@gmail.com>
2025-11-28 12:23:18 +0300

    mail için tls ayarı yapıldı

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -6,9 +6,13 @@ import { env } from '../utils/env.js';
 const transporter = nodemailer.createTransport({
   host: env(SMTP.SMTP_HOST),
   port: Number(env(SMTP.SMTP_PORT)),
+  secure: false,
   auth: {
     user: env(SMTP.SMTP_USER),
     pass: env(SMTP.SMTP_PASSWORD),
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
